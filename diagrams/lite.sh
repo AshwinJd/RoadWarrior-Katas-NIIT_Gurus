ea3218fe26a21aead44f67d601f70b845a6403f7
Sagar Patke <sagarpatkeatl@gmail.com>
2023-09-16 15:25:54 -0400

    ADR Integration

--- diagrams/lite.sh
+++ diagrams/lite.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 pwd
 set -e
 
-docker run -it --rm -p 8080:8080 -v `pwd`:/usr/local/structurizr structurizr/lite
+docker run -it --rm -p 8080:8080 -v `pwd`:/usr/local/structurizr -v `pwd`/doc:/usr/local/structurizr/doc structurizr/lite
--- diagrams/workspace.dsl
+++ diagrams/workspace.dsl
@@ -1,4 +1,5 @@
 workspace {
+    !adrs doc/adr
 
     model {
 
